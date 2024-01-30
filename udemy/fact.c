1db68932e593425109bd0cf8a9dd50812986601e
Ajaykrishna2001 <114055725+Ajaykrishna2001@users.noreply.github.com>
2024-01-30 12:21:58 +0530

    Add files via upload

---
+++ udemy/fact.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int c,fact=1;
+    printf("Enter the number");
+    scanf("%d",&f);
+    for(int i=0;i<=f;i++){
+        fact=fact*i;
+    }
+    printf("The prime number is %d",fact);
+}
