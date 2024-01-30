762e34a70dfcbe497dcfa5f3882d2ee2f6828e9d
Ajaykrishna2001 <114055725+Ajaykrishna2001@users.noreply.github.com>
2024-01-30 12:12:01 +0530

    Add files via upload

---
+++ Javascript small tasks/JS/script.js
@@ -0,0 +1,33 @@
+//alert("welcome to Js from external file");
+
+/*console.log("this is will show in console");
+console.log(12546);
+console.log(18.2);
+console.log([57,78,12,33,09]);
+console.log({fname:'ajay',age:22});
+console.table({fname:'ajay',age:22});
+console.error("sample Error");
+console.warn("custom sample Error");
+console.clear();
+console.time("Timer");
+for(i=0;i<10;i++)
+{
+    console.log(i);
+}
+console.timeEnd("Timer");*/
+
+/*
+1997
+var
+2015 E6
+let
+const
+*/
+
+if(true)
+{
+    var msg="welcome to my you tube channel";
+}
+console.log(msg);
+
+
